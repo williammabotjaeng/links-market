3183feb2739250f67b1f7cdec04bf13f5068dfc0
williammabotjaeng <williammabotjaeng@gmail.com>
2024-12-15 07:31:57 +0200

    poppins, site wide font

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!--<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css">-->
     @vite(['resources/css/app.css', 'resources/js/app.js']) 
     <title>@yield('title', 'Where Publishers Meet Buyers—Effortlessly')</title>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
