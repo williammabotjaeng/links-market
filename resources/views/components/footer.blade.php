b3569ba424e26d1bb28eef87a9a500f8371e1e66
williammabotjaeng <williammabotjaeng@gmail.com>
2024-12-15 08:42:38 +0200

    position, fixed, bottom 0, width 100%, footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="footer">
+<footer class="footer" style="position: fixed; bottom: 0px; width: 100%">
     <div class="content has-text-centered">
         <p>&copy; {{ date('Y') }} LinksMarket. Where Publishers Meet Buyers—Effortlessly.</p>
     </div>
