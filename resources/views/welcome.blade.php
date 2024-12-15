8fcfe818a3ab59f2df8f32ffe933b18b69361041
williammabotjaeng <williammabotjaeng@gmail.com>
2024-12-15 11:17:22 +0200

    LinksMarket, title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite(['resources/css/app.css', 'resources/js/app.js']) 
     <link rel="shortcut icon" href="{{ asset('icon.png') }}" type="image/x-icon">
-    <title>@yield('title', 'Where Publishers Meet Buyers—Effortlessly')</title>
+    <title>@yield('title', 'LinksMarket | Where Publishers Meet Buyers—Effortlessly')</title>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
