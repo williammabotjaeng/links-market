e78c568b8d551ae297e5334ccbce578077e57e12
williammabotjaeng <williammabotjaeng@gmail.com>
2024-12-15 10:31:44 +0200

    precise bg size

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,12 @@
 
 @section('content')
     <div>
-        <div class="is-centered has-text-centered is-justify-content-center is-align-content-center" style="background-color: rgba(0, 140, 140, 0.5); height: 100vh">
+        <div class="is-centered has-text-centered is-justify-content-center is-align-content-center" style="background-color: rgba(0, 140, 140, 0.5); height: 90vh">
             <div class="container has-text-centered">
                 <h1 class="title has-text-white">Welcome to LinksMarket!</h1>
                 <p class="subtitle has-text-white">Where Publishers Meet Buyers—Effortlessly.</p>
                 <p class="has-text-white">Explore our features and services tailored for publishers and buyers.</p>
+                <br>
                 <a class="button is-large is-light" href="{{ route('register') }}">Let's Get Started</a>
             </div>
         </div>
