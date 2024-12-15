91a42b17106032ff3070f2b970572cfd118bb9d4
williammabotjaeng <williammabotjaeng@gmail.com>
2024-12-15 08:46:18 +0200

    has-shadow, header, footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="footer" style="position: fixed; bottom: 0px; width: 100%">
+<footer class="footer has-shadow" style="position: fixed; bottom: 0px; width: 100%; border-top: 1px dotted rgba(0, 140, 140, 0.5)">
     <div class="content has-text-centered">
         <p>&copy; {{ date('Y') }} LinksMarket. Where Publishers Meet Buyers—Effortlessly.</p>
     </div>
