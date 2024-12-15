d8036866738554cb1fda006528c2274e2634e5a9
williammabotjaeng <williammabotjaeng@gmail.com>
2024-12-15 07:45:47 +0200

    favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite(['resources/css/app.css', 'resources/js/app.js']) 
+    <link rel="shortcut icon" href="{{ asset('icon.png') }}" type="image/x-icon">
     <title>@yield('title', 'Where Publishers Meet Buyers—Effortlessly')</title>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
