6dbbf55c44e1a80744ca9b01d83c8d1e6efc9ff6
williammabotjaeng <williammabotjaeng@gmail.com>
2024-12-18 21:30:20 +0200

    reserved view, account

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -11,18 +11,21 @@ class AccountController extends Controller
     public function showBalance()
     {
         $account = Account::where('user_id', Auth::id())->first();
-        return view('dashboard.accounts.balance', compact('account'));
+        $user = Auth::user();
+        return view('dashboard.accounts.balance', compact('account', 'user'));
     }
 
     public function showReserved()
     {
         $account = Account::where('user_id', Auth::id())->first();
-        return view('dashboard.accounts.reserved', compact('account'));
+        $user = Auth::user();
+        return view('dashboard.accounts.reserved', compact('account', 'user'));
     }
 
     public function showBonus()
     {
         $account = Account::where('user_id', Auth::id())->first();
-        return view('dashboard.accounts.bonus', compact('account'));
+        $user = Auth::user();
+        return view('dashboard.accounts.bonus', compact('account', 'user'));
     }
 }
