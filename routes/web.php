0d4a88bcecb2a61e7bfa8a7007a7031de07d9e8d
williammabotjaeng <williammabotjaeng@gmail.com>
2024-12-18 10:15:46 +0200

    Tasks, TaskController, web:

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\WebsiteController;
+use App\Http\Controllers\TaskController;
 
 // Welcome Page
 Route::get('/', function () {
