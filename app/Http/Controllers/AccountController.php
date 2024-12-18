dce6475be3aaab310246bd4ba9f553e5e052c5bc
williammabotjaeng <williammabotjaeng@gmail.com>
2024-12-18 20:32:24 +0200

    Auth, Accounts, balance view

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Account;
+use Illuminate\Support\Facades\Auth;
 
 class AccountController extends Controller
 {
     public function showBalance()
     {
         $account = Account::where('user_id', Auth::id())->first();
-        return view('dashboard.account.balance', compact('account'));
+        return view('dashboard.accounts.balance', compact('account'));
     }
 
     public function showReserved()
