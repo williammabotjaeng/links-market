98df210a8d83cf469d6cd4aa7878aa50d697ea4a
williammabotjaeng <williammabotjaeng@gmail.com>
2024-12-20 09:18:05 +0200

    socials, index, user, Auth import

--- app/Http/Controllers/SocialController.php
+++ app/Http/Controllers/SocialController.php
@@ -5,22 +5,25 @@ namespace App\Http\Controllers;
 use App\Models\Social;
 use App\Models\Account;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class SocialController extends Controller
 {
     // Display a listing of the socials
     public function index()
     {
+        $user = Auth::user();
         $socials = Social::all();
         $account = Account::where('user_id', Auth::id())->first();
-        return view('dashboard.socials.index', compact('socials', 'account'));
+        return view('dashboard.socials.index', compact('socials', 'account', 'user'));
     }
 
     // Show the form for creating a new social
     public function create()
     {
+        $user = Auth::user();
         $account = Account::where('user_id', Auth::id())->first();
-        return view('socials.create', compact('account'));
+        return view('socials.create', compact('account', 'user'));
     }
 
     // Store a newly created social in storage
@@ -38,15 +41,17 @@ class SocialController extends Controller
     // Display the specified social
     public function show(Social $social)
     {
+        $user = Auth::user();
         $account = Account::where('user_id', Auth::id())->first();
-        return view('socials.show', compact('social', 'account'));
+        return view('socials.show', compact('social', 'account', 'user'));
     }
 
     // Show the form for editing the specified social
     public function edit(Social $social)
     {
+        $user = Auth::user();
         $account = Account::where('user_id', Auth::id())->first();
-        return view('socials.edit', compact('social', 'account'));
+        return view('socials.edit', compact('social', 'account', 'user'));
     }
 
     // Update the specified social in storage
@@ -56,7 +61,7 @@ class SocialController extends Controller
             'platform' => 'required|string|max:255',
             'account_handle' => 'required|string|max:255',
         ]);
-
+        
         $social->update($request->all());
         $account = Account::where('user_id', Auth::id())->first();
         return redirect()->route('socials.index')->with('success', 'Social account updated successfully.');
